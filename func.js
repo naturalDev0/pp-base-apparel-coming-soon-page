6a7f589c2d57110ba8399395012d7aebe76296ef
Tommy Lee <tonglin.lee@gmail.com>
2020-07-04 15:28:46 +0800

    Made changes to form to return validation function 'emailValid' in index.html, and provide logics through func.js to output the results to index.html

--- func.js
+++ func.js
@@ -1,1 +1,19 @@
 // Add in functions later on for email input validation
+
+function emailValid() {
+    
+    let value = document.getElementById("email").value,
+        error = document.getElementById("error-msg"),
+        text = "";
+
+    console.log(value);
+    console.log(error);
+
+    if (value === "") { 
+        error.innerHTML = "Please provide an email";
+        return false;
+    } else {
+        error.innerHTML = "Input ok";
+        return true;
+    }
+}
