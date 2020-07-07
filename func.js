eebb740df1a095abe179f47a0cb6dfecca243d31
Tommy Lee <tonglin.lee@gmail.com>
2020-07-07 20:01:59 +0800

    Added Regular Expression and set condition for the written RegEx testing if it matches the input.

--- func.js
+++ func.js
@@ -1,13 +1,15 @@
 // Add in functions later on for email input validation
 
+const re = /^(\w+)@(\w+).*(\w+)$/;  // email validation
+
 // Validating email
 function emailValid() {
     
-    let value = document.getElementById("email").value,
+    let email = document.getElementById("email").value,
         error = document.getElementById("error-msg");
 
-    // Check if value is empty
-    if (value === "") { 
+    // Check if 'email' value is correct validation
+    if (!re.test(email)) { 
         
         // Output result towards HTML page
         error.innerHTML = "Please provide a valid email";
