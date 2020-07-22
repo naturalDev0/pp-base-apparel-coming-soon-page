f67f6d9962ca021ac882984db54d9b1f784df6c4
Tommy Lee <tonglin.lee@gmail.com>
2020-07-22 11:22:34 +0800

    Replace normal from validation with addEventListner to prevent page from refreshing when results equals error.

--- func.js
+++ func.js
@@ -1,21 +1,64 @@
 // Add in functions later on for email input validation
 
-const re = /^(\w+)@(\w+).*(\w+)$/;  // email validation
+const re = /^(\w+)@(\w+).*(\w+)$/;  // email validation regex
+const form = document.querySelector(".form");
+const errmsg = document.querySelector("#error-msg");
 
 // Validating email
-function emailValid() {
-    
-    let email = document.getElementById("email").value,
-        error = document.getElementById("error-msg");
+document.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let email = document.getElementById("email").value;        
 
     // Check if 'email' value is correct validation
     if (!re.test(email)) { 
         
         // Output result towards HTML page
-        error.innerHTML = "Please provide a valid email";
-        return false;   // return the status to form
+        errmsg.innerHTML = "Please provide a valid email";
+        form.classList.add("error");
+        // return false;   // return the status to form
     } else {
-        error.innerHTML = "";
-        return true;
+        errmsg.innerHTML = "";
+        form.classList.remove("error");
+        console.log("Email submitted");
+        // return true;
     }
-}
+
+    document.getElementById("email").value = "";
+});
+
+
+// function emailValid() {
+    
+//     let email = document.getElementById("email").value,
+//         error = document.getElementById("error-msg");
+
+//     // Check if 'email' value is correct validation
+//     if (!re.test(email)) { 
+        
+//         // Output result towards HTML page
+//         error.innerHTML = "Please provide a valid email";
+//         return false;   // return the status to form
+//     } else {
+//         error.innerHTML = "";
+//         return true;
+//     }
+
+//     // e.preventDefault();
+      
+//     // if (emailIsValid($input.value)) {
+//     //     $form.classList.remove("error");
+//     //     $error.style.visibility = "hidden";
+//     //     alert("Thank you for signing up!");
+//     // } else {
+//     //     $form.classList.add("error");
+//     //     $error.style.visibility = "visible";
+//     // }
+    
+//     // $input.value = "";
+
+//     // $form.addEventListener("submit", function (e) {
+        
+//     // });
+
+// }
