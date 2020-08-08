b81c2c600cad641457352a6240361eb4d9f057de
Tommy Lee <tonglin.lee@gmail.com>
2020-08-08 19:23:39 +0800

    1. Added error alert icon in index.html, style.css and func.js
    ---> style.css,
    1. Change '#email' id to '.email' class for utilization of querySelector in func.js.
    2. Added 'input.err-icon' class for toggling error alert icon when user failed to input correct email format.
    
    ---> func.js
    1. Added 'errIcon' constant to preselect '.email' class in style.css for toggling '.err-icon' class in the UI.

--- func.js
+++ func.js
@@ -3,62 +3,29 @@
 const re = /^(\w+)@(\w+).*(\w+)$/;  // email validation regex
 const form = document.querySelector(".form");
 const errmsg = document.querySelector("#error-msg");
+const errIcon = document.querySelector(".email");
 
 // Validating email
 document.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    let email = document.getElementById("email").value;        
+    let email = errIcon.value;
 
     // Check if 'email' value is correct validation
     if (!re.test(email)) { 
         
         // Output result towards HTML page
         errmsg.innerHTML = "Please provide a valid email";
-        form.classList.add("error");
+        errIcon.classList.add("err-icon");
+        // form.classList.add("error");
         // return false;   // return the status to form
     } else {
         errmsg.innerHTML = "";
-        form.classList.remove("error");
+        errIcon.classList.remove("err-icon");
+        // form.classList.remove("error");
         console.log("Email submitted");
         // return true;
     }
 
-    document.getElementById("email").value = "";
+    errIcon.value = "";
 });
-
-
-// function emailValid() {
-    
-//     let email = document.getElementById("email").value,
-//         error = document.getElementById("error-msg");
-
-//     // Check if 'email' value is correct validation
-//     if (!re.test(email)) { 
-        
-//         // Output result towards HTML page
-//         error.innerHTML = "Please provide a valid email";
-//         return false;   // return the status to form
-//     } else {
-//         error.innerHTML = "";
-//         return true;
-//     }
-
-//     // e.preventDefault();
-      
-//     // if (emailIsValid($input.value)) {
-//     //     $form.classList.remove("error");
-//     //     $error.style.visibility = "hidden";
-//     //     alert("Thank you for signing up!");
-//     // } else {
-//     //     $form.classList.add("error");
-//     //     $error.style.visibility = "visible";
-//     // }
-    
-//     // $input.value = "";
-
-//     // $form.addEventListener("submit", function (e) {
-        
-//     // });
-
-// }
